3f3eb676e32131fef1eb455f9d4f18d3aefb69d8
JeppesGitHub <jreink22@student.aau.dk>
2023-04-06 10:49:11 +0200

    Update

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import router from './routes.js';
-import fs from 'fs'
-import https from 'https'
 const app = express();
+const http = express();
+import router from './routes.js';
+import https from "https";
+import fs from "fs";
+
+const privateKey = fs.readFileSync("/etc/letsencrypt/live/mealmate.otterhosting.net/privkey.pem");
+const certificate = fs.readFileSync("/etc/letsencrypt/live/mealmate.otterhosting.net/cert.pem");
 
 app.use(express.static("public"));
 app.set("port", process.env.PORT || 3000);
@@ -12,9 +16,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
 
 
+http.get('*', function (req, res) {
+    res.redirect('https://' + req.headers.host + req.url);
+})
 
 
-
-app.listen(app.get('port'), function () {
+http.listen(app.get('port'), function () {
     console.log('app listening at: ' + "http://localhost:" + app.get('port') + "/");
+});
+
+https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app).listen("443", function () {
+    console.log(`port 443 at ` + "mealmate.otterhosting.net");
 });
